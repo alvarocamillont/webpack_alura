02110acd85354de5063a2b4fec9f049255b7f3ba
Alvaro <alvaro.camilloneto@gmail.com>
2018-09-10 23:55:26 -0300

    Carregamento de css via módulo npm

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -8,23 +8,42 @@ if(process.env.NODE_ENV == 'production') {
 }
 
 module.exports = {
-  entry: './app-src/app.js',
-  output: {
-      filename: 'bundle.js',
-      path: path.resolve(__dirname, 'dist'),
-      publicPath:'dist'
-  },
-  module: {
-          rules: [
-              {
-                  test:/\.js$/,
-                  exclude: /node_modules/,
-                  use: {
-                      loader: 'babel-loader'
-                  }
-
-              },
-          ]
-      },
-      plugins
+    entry: './app-src/app.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+        publicPath: "dist"
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            },
+            { 
+                test: /\.css$/, 
+                loader: 'style-loader!css-loader' 
+            },
+            { 
+                test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/, 
+                loader: 'url-loader?limit=10000&mimetype=application/font-woff' 
+            },
+            { 
+                test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, 
+                loader: 'url-loader?limit=10000&mimetype=application/octet-stream'
+            },
+            { 
+                test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, 
+                loader: 'file-loader' 
+            },
+            { 
+                test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, 
+                loader: 'url-loader?limit=10000&mimetype=image/svg+xml' 
+            }            
+        ]
+    },
+    plugins
 }
